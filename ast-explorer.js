3dbaf21d4dcdb054f7a313d65a5a9fd481be2da2
Sergey Sova <mail@sergeysova.com>
2021-03-27 14:12:30 +0300

    feat: resolve import declaration

--- ast-explorer.js
+++ ast-explorer.js
@@ -30,42 +30,73 @@ export default function (babel) {
       //},
 
       TaggedTemplateExpression(path, state) {
-        const derivedName = determineName(t, path);
-        const sid = generateStableID(
-          '',
-          '',
-          derivedName,
-          path.node.loc.start.line,
-          path.node.loc.start.column,
-        );
-        const fullName = createName(sid, derivedName);
+        // Find original import for current template tag
         const tagName = path.node.tag.name;
         const binding = path.scope.getOwnBinding(tagName);
         if (binding) {
-          const module = binding.path.find((item) =>
-            item.isImportDeclaration(),
-          );
-          if (module && allowedModules.includes(module.node.source.value)) {
-            //path.scope.rename(tagName);
-            console.log(path, fullName, module);
-            path.replaceWith(
-              t.objectExpression([
-                t.objectProperty(
-                  t.identifier('content'),
-                  t.stringLiteral('COMPILED CSS HERE'),
-                ),
-                t.objectProperty(
-                  t.identifier(tagName),
-                  t.stringLiteral(fullName),
-                ),
-              ]),
-            );
-            // console.log(Object.keys(path.__proto__).sort())
+          const resolved = resolveOriginalImport(t, binding);
+          if (resolved) {
+            const { module, name } = resolved;
+            // Check that template tag imported from supported module
+            if (allowedModules.includes(module.node.source.value)) {
+              // Create stable unique id with readable name
+              const derivedName = determineName(t, path);
+              const sid = generateStableID(
+                '',
+                '',
+                derivedName,
+                path.node.loc.start.line,
+                path.node.loc.start.column,
+              );
+              const fullName = createName(sid, derivedName);
+
+              //path.scope.rename(name);
+              console.log(path, fullName, module);
+              path.replaceWith(
+                t.objectExpression([
+                  t.objectProperty(
+                    t.identifier('content'),
+                    t.stringLiteral('COMPILED CSS HERE'),
+                  ),
+                  t.objectProperty(
+                    t.identifier(name),
+                    t.stringLiteral(fullName),
+                  ),
+                ]),
+              );
+              // console.log(Object.keys(path.__proto__).sort())
+            }
           }
         }
       },
     },
   };
+}
+
+/**
+ * Find import declaration for binding and resolve original name
+ * For example we have next two lines:
+ * import { foo as bar } from 'module';
+ * const demo = bar``
+ *
+ * This function allows to resolve `foo` from `bar` usage
+ * with import declaration
+ */
+function resolveOriginalImport(t, binding) {
+  const local = binding.identifier;
+  const module = binding.path.find((path) => path.isImportDeclaration());
+  if (!module) return null;
+
+  const isImportedByValue = module.node.importKind === 'value';
+  if (!isImportedByValue) return null;
+
+  const specifier = module.node.specifiers
+    .filter((node) => t.isImportSpecifier(node))
+    .find((node) => node.local.name === local.name);
+
+  if (!specifier) return null;
+
+  return { name: specifier.imported.name, module };
 }
 
 function createName(sid, determined) {
--- example.js
+++ example.js
@@ -1,4 +1,4 @@
-//https://astexplorer.net/#/gist/68f6ac120f7543e3efc6a43b0904e107/ab6b50e36e1ebc79938d80b6d9547dac9d6465e2
+//https://astexplorer.net/#/gist/68f6ac120f7543e3efc6a43b0904e107/9c084c624403c8dac19bce8fc3b3f8f4752f7271
 https: import { css, keyframes, createGlobalStyle } from 'foliage-react';
 
 const padding = css`
